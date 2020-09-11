cbcb70f275859ab1d48e4883d424e2871314ecdc
Drahoslav Zan <zandrahoslav@gmail.com>
2020-09-11 23:25:59 +0200

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gatsby Template Starter
+
+Gatsby template with Typeascript and TailwindCSS. Bundled with server in Go.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   purge: [
     "./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx",
   ],
